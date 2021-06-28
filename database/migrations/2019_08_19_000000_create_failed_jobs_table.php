34ba9b0c55f1f6316c2f6b60cbbf140575bf4314
Pavel Rudenko <48448071+SantaRiver@users.noreply.github.com>
2021-06-28 20:38:02 +0500

    fix migration & change config

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,7 +13,7 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        if (Schema::hasTable('password_resets')) {
+        if (!Schema::hasTable('password_resets')) {
             Schema::create(
                 'failed_jobs',
                 function (Blueprint $table) {
